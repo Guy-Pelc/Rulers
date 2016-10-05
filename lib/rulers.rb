acf7ca40668c21b12be3e760d39229dbe922bc24
Guy-Pelc <guy.pelc@gmail.com>
2016-10-05 16:37:21 -0400

    updated controllers and routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,10 +4,20 @@ require "rulers/array"
 module Rulers
   # Your code goes here...
   class Application
-  	def call(env)
-  		'echo debug>debug.txt';
-  		[200,{'Content-Type'=>'text/html'},
-  			["Hello from Ruby on Rulers!"]]
-  	end
+  		def call(env)
+  		klass,act=get_controller_and_action(env)
+  		controller=klass.new(env)
+  		text=controller.send(act)
+  		[200,{'content-Type'=>'text/html'},
+  			[text]]
+  		end
   end
+  class Controller
+  		def initialize(env)
+  		@env=env
+  		end
+  		def env
+  		@env
+  		end
+  	end
 end
